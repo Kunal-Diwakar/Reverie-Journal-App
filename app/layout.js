ad36104fd5b8a97ab53268eb5c401b1eeea3a77f
Kunal Diwakar <kunaldiwakar1999@gmail.com>
2025-05-10 01:13:34 +0530

    Bugs Fixing -3

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
        <ReactLenis root>
       <html lang="en">
         <body className={`${bricolage.className} ${amarante.variable} antialiased`}>
